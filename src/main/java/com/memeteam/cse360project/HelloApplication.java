4c3253e4fb596789a6815abc404202e84808a021
Glowstick // Robbie <robmarracino@gmail.com>
2021-09-23 07:33:54 -0700

    some docs

--- src/main/java/com/memeteam/cse360project/HelloApplication.java
+++ src/main/java/com/memeteam/cse360project/HelloApplication.java
@@ -10,10 +10,23 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        // load resources for scene
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        // define scene
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        // change scene title (toolbar title)
         stage.setTitle("Hello!");
+
+        // set window to always display above all other windows
+        stage.setAlwaysOnTop(true);
+
+        // set the scene of the stage to what was defined
         stage.setScene(scene);
+
+        // display
         stage.show();
     }
 
